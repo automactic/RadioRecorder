31c6aefae2200d3770ba56edfaa72ffcd76d5e13
ChrisLi <chris@kiwix.org>
2021-03-25 12:11:30 -0400

    recorder

--- app/main.py
+++ app/main.py
@@ -2,15 +2,12 @@ import asyncio
 
 import aiohttp
 
-from  recorder import Recorder
+from recorders import TuneinStationRecorder
 
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        headers = {'id': 's297990', 'formats': 'hls'}
-        async with session.get('http://opml.radiotime.com/Tune.ashx', params=headers) as response:
-            url = await response.text()
-            await Recorder(url, session).record()
+        await TuneinStationRecorder('s297990', session).record()
 
 if __name__ == '__main__':
     asyncio.run(main())
